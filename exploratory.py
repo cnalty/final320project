1731433eb25bf801b18c1220013474b2cc0040e5
Chris Nalty <cnaltypga@gmail.com>
2019-12-14 11:44:17 -0500

    added more graphing and explainations

--- exploratory.py
+++ exploratory.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import pickle
 import matplotlib.pyplot as plt
-
+import matplotlib.patches as mpatches
 
 ranks = pd.read_csv("apranks.csv") # found here https://www.sports-reference.com/cbb/seasons/2019-polls.html
 
@@ -27,11 +27,13 @@ for elem in ranks.columns:
 print(new_ranks.head())
 
 # now we transpose the table to have the dates as the index as desired and add the year
+# We're also going to rename the Final column to 3/18/2019, the date they were released
 
 new_ranks = new_ranks.transpose()
 
 new_ranks = new_ranks.rename(index=lambda x: re.sub("^0.*", x + "/2019", x))
 new_ranks = new_ranks.rename(index=lambda x: re.sub("^1.*", x + "/2018", x))
+new_ranks = new_ranks.rename(index=lambda x: re.sub("Final", '03/18/2019', x))
 
 print(new_ranks.head())
 
@@ -58,13 +60,52 @@ print(rank1s)
 # Next lets graph some of the stats for rank #1 that are likely important to them winning
 stats_to_graph = ["APG", "BKPG", "RPG", "FG%", "PFPG", "SCR MAR", "STPG", "3FG%"]
 t_cols = {'Duke':'b', 'Gonzaga':'g', 'Kansas':'r', 'Tennessee':'m'}
+
+# we need to create a custom legend so points Teams aren't repeated
+
+patches = [mpatches.Patch(color=v, label=k) for k, v in t_cols.items()]
+
 for stat in stats_to_graph:
     for index in rank1s.index:
         if index in dfs.keys():
             curr_y = dfs[index].loc[dfs[index]['Name'] == rank1s[index]][stat].values[0]
-            plt.scatter(index, curr_y, c=t_cols[rank1s[index]], label=rank1s[index])
-            plt.annotate(rank1s[index], (index, curr_y))
-    #plt.legend(t_cols.keys(), t_cols.values())
-    plt.title(stat)
+            plt.scatter(index, curr_y, c=t_cols[rank1s[index]])
 
+    plt.title(stat)
+    plt.legend(handles=patches)
     plt.show()
+
+''' From these graphs, if one stat is very important to being rank 1, we'd expect all the teams to have a very similar
+    value for such stats. However it seems all of the stats, except 3-point field goal percentage and rebounds per game,
+    have a very large variations in values for week to week. We can see however that each team tends to keep fairly 
+    consistent on each stat from week to week. 3-point field goal percentage being more consistent would lead us to 
+    believe this is a more important stat to being rank one than others. 
+    
+    Next we should plot these values alongside the average for the Division each week to see how rank 1s stack up.'''
+
+stats_to_graph = ["APG", "BKPG", "RPG", "FG%", "PFPG", "SCR MAR", "STPG", "3FG%"]
+t_cols = {'Duke':'b', 'Gonzaga':'g', 'Kansas':'r', 'Tennessee':'m', 'Average': 'k'}
+
+# we'll use the same base code as prior, but add a lineplot of the averages
+
+patches = [mpatches.Patch(color=v, label=k) for k, v in t_cols.items()]
+
+for stat in stats_to_graph:
+    dates = []
+    avgs = []
+    for index in rank1s.index:
+        if index in dfs.keys():
+            dates.append(index)
+            curr_y = dfs[index].loc[dfs[index]['Name'] == rank1s[index]][stat].values[0]
+            plt.scatter(index, curr_y, c=t_cols[rank1s[index]])
+
+            avgs.append(dfs[index][stat].mean())
+    plt.plot(dates, avgs, c=t_cols['Average'])
+    plt.title(stat)
+    plt.legend(handles=patches)
+    plt.show()
+
+'''We can see that even though the number one ranked team doesn't seem to lean on any one stat they excel comparted to 
+    average team in Division 1. There are are a couple stats, such as Steals Per Game, where Tennessee is worse than the
+    average, and 3-point field goal percentage where Duke is below average in the later half of the season, but outside
+    these two occurrences it's clear these teams outperform most of the Division'''
